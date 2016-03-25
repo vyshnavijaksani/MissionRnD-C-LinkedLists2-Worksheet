2a20b2562b0b0ec4ffe6dcd92176f4515ac278cd
vyshnavijaksani <vyshnavijaksani@missionrnd.com>
2016-03-25 10:59:29 +0530

    Submit Commit

--- src/merge2LinkedLists.cpp
+++ src/merge2LinkedLists.cpp
@@ -12,12 +12,48 @@ NOTES:
 */
 
 #include <stdio.h>
-
+#include<malloc.h>
 struct node {
 	int num;
 	struct node *next;
 };
+void insertnode(struct node** end, struct node** start)
+{
+	struct node* newNode = *start;
+	if (newNode != NULL){ ; }
+	*start = newNode->next;
+	newNode->next = *end;
+	*end = newNode;
+}
 
 struct node * merge2LinkedLists(struct node *head1, struct node *head2) {
-	return NULL;
+	
+		struct node* result = NULL;
+		struct node** res = &result;
+
+		while (true)
+		{
+			if (head1 == NULL)
+			{
+				*res = head2;
+				break;
+			}
+			else if (head2 == NULL)
+			{
+				*res = head1;
+				break;
+			}
+			if (head1->num <= head2->num)
+			{
+				insertnode(res, &head1);
+			}
+			else
+			{
+				insertnode(res, &head2);
+			}
+			//again pointing result to first
+			res = &((*res)->next);
+		}
+		return(result);
 }
+
